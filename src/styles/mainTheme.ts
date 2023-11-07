359f9036e3be9ad724120630d3d1ca5e89e548f1
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-07 20:00:06 +0100

    Add global style & main theme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const maintTheme: DefaultTheme = {
+  color: {
+    mainDarkColor: "#000",
+    mainLightColor: "#fff",
+  },
+  typography: {
+    mainFontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+  },
+};
+
+export default maintTheme;
