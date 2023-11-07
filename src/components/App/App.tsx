a483da77008f0d6db4b8dfa81d41a0c15698b249
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-07 11:46:45 +0100

    Modify html tag names

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import MoviesListPage from "../../pages/MoviesListPage";
 
 const App = (): React.ReactElement => (
-  <>
+  <div>
     <Routes>
       <Route path="/" element={<Navigate to="/movies" />} />
       <Route path="/movies" element={<MoviesListPage />} />
     </Routes>
-  </>
+  </div>
 );
 
 export default App;
--- src/pages/MoviesListPage.tsx
+++ src/pages/MoviesListPage.tsx
@@ -1,9 +1,7 @@
 const MoviesListPage = (): React.ReactElement => (
-  <>
-    <header className="header__title">
-      <h1>My favorite horror movies</h1>
-    </header>
-  </>
+  <header className="header__title">
+    <h1>My favorite horror movies</h1>
+  </header>
 );
 
 export default MoviesListPage;
