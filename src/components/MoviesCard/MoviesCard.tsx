9a2329c1a74adc42758c310687767807eca2d751
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-08 23:25:04 +0100

    Add watchedMovie function & styles to card

--- src/components/MoviesCard/MoviesCard.tsx
+++ src/components/MoviesCard/MoviesCard.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { Movies } from "../../features/movies/types";
+import { toggleWatchedMovieActionCreator } from "../../features/movies/moviesSlice";
 import MoviesCardStyled from "./MoviesCardStyled";
+import useMoviesApi from "../../hooks/useMoviesApi";
 
 interface MoviesProps {
   movie: Movies;
 }
 
-const MovieCard = ({ movie }: MoviesProps): React.ReactElement => (
-  <MoviesCardStyled>
-    <img
-      src={movie.image}
-      alt={movie.name}
-      className="movie__image"
-      width={300}
-      height={450}
-    />
-    <h2 className="movie__title">{movie.name}</h2>
-    <span>{`Year: ${movie.year}`}</span>
-  </MoviesCardStyled>
-);
+const MovieCard = ({ movie }: MoviesProps): React.ReactElement => {
+  const dispatch = useDispatch();
+  const { setWatchMovie } = useMoviesApi();
+
+  const changeWatchMovie = useCallback(() => {
+    setWatchMovie(movie.id, movie.isWatched);
+    dispatch(toggleWatchedMovieActionCreator(movie.id));
+  }, [dispatch, movie.id, movie.isWatched, setWatchMovie]);
+
+  return (
+    <MoviesCardStyled>
+      <img
+        src={movie.image}
+        alt={movie.name}
+        className="movie__image"
+        width={300}
+        height={450}
+      />
+      <h2 className="movie__title">{movie.name}</h2>
+      <span>{`Year: ${movie.year}`}</span>
+      <label className="movie__label">
+        Watched
+        <input
+          className="movie__checkbox"
+          type="checkbox"
+          checked={movie.isWatched}
+          onChange={changeWatchMovie}
+        />
+      </label>
+    </MoviesCardStyled>
+  );
+};
 
 export default MovieCard;
