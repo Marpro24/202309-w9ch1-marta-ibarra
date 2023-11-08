94f6a26adbdb6e89272f681af614ad6680b084ba
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-08 23:23:50 +0100

    Add setWatchMovie function

--- src/hooks/useMoviesApi.tsx
+++ src/hooks/useMoviesApi.tsx
@@ -11,9 +11,26 @@ const useMoviesApi = () => {
     return movies;
   }, [apiURL]);
 
-  return {
-    getMovies,
-  };
+  const setWatchMovie = useCallback(
+    async (movieId: number, isWatched: boolean): Promise<boolean> => {
+      const response = await fetch(`${apiURL}/movies/${movieId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isWatched: !isWatched }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sorry, we couldn't process your request");
+      }
+
+      return isWatched;
+    },
+    [apiURL],
+  );
+
+  return { getMovies, setWatchMovie };
 };
 
 export default useMoviesApi;
