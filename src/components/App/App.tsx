3c928ccaf700648e55277738dfa4e25662e5a7ed
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-09 22:18:26 +0100

    Add MovieForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import MoviesListPage from "../../pages/MoviesListPage";
+import MoviesListPage from "../../pages/MoviesListPage/MoviesListPage";
 import AppStyled from "./AppStyled";
+import MovieFormPage from "../../pages/MovieFormPage/MovieFormPage";
 
 const App = (): React.ReactElement => (
   <AppStyled>
     <Routes>
       <Route path="/" element={<Navigate to="/movies" />} />
       <Route path="/movies" element={<MoviesListPage />} />
+      <Route path="/movie-form" element={<MovieFormPage />} />
     </Routes>
   </AppStyled>
 );
