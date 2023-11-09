e2f1082f61d6ee7be7d53cfb96ab98fd830af188
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-09 22:15:03 +0100

    Add MovieForm component

---
+++ src/components/MovieForm/MovieForm.tsx
@@ -0,0 +1,29 @@
+import MoviesFormStyled from "./MovieFormStyled";
+
+const movieForm = (): React.ReactElement => {
+  return (
+    <MoviesFormStyled>
+      <label className="form__label" htmlFor="title">
+        Title:
+      </label>
+      <input className="form__input" type="text" id="title" required />
+      <label className="form__label" htmlFor="year">
+        Year:
+      </label>
+      <input className="form__input" type="number" id="year" required />
+      <label className="form__label" htmlFor="image">
+        Add and image:
+      </label>
+      <input className="form__input" type="url" id="website" required />
+      <label className="form__label" htmlFor="watched">
+        Have you seen it?
+      </label>
+      <input type="checkbox" id="watched" required />
+      <button className="form__button" type="submit">
+        Send
+      </button>
+    </MoviesFormStyled>
+  );
+};
+
+export default movieForm;
