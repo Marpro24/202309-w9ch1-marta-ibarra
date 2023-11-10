7659cab991e549079d2133495eb8bfefcf0df9ca
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-10 10:22:43 +0100

    Add styles to MovieForm

--- src/components/MovieForm/MovieFormStyled.ts
+++ src/components/MovieForm/MovieFormStyled.ts
@@ -5,6 +5,13 @@ const MoviesFormStyled = styled.form`
   flex-direction: column;
   align-items: flex-start;
   gap: 10px;
+  margin: 35px;
+  display: flex;
+  flex-direction: column;
+  border: solid;
+  border-color: #000;
+  border-radius: 30px;
+  padding: 30px;
 
   .form__input {
     padding: 5px;
@@ -19,6 +26,8 @@ const MoviesFormStyled = styled.form`
     color: #fff;
     height: 40px;
     width: 90px;
+    border-radius: 30px;
+    border-color: #000;
   }
 `;
 
